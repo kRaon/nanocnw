716e9a0b0b13d976cfc13011a6a9f025e7c95250
kRaon <dydgus2796@gmail.com>
2022-05-17 13:51:45 +0900

    Update ie_alert.js

--- ie_alert.js
+++ ie_alert.js
@@ -4,6 +4,6 @@ const trident = window.navigator.userAgent.search('Trident');
 
   if (trident !== -1 || msie !== -1) {
     alert(
-      '인터넷익스프로러는 지원하지 않는 브라우저입니다.  \n크롬, 파이어폭스, 엣지, 웨일 브라우저를 사용 부탁드립니다.'
+      '인터넷 익스플로러는 지원하지 않는 브라우저입니다.  \n크롬, 파이어폭스, 엣지, 웨일 브라우저를 사용 부탁드립니다.'
     );
   }
